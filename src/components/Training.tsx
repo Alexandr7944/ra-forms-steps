cc379f82dd0727941e2aac5bdfc30cb85449d7ae
Alexander Vasnev <a-s-vasnev88@yandex.ru>
2023-05-26 16:44:46 +0300

    initial comit

---
+++ src/components/Training.tsx
@@ -0,0 +1,28 @@
+import { ITraining } from "../App"
+
+const Training: React.FC<TrainingProps> = ({ training, remuveTraining }) => {
+  const date = training.date && new Date(training.date).toLocaleDateString() || '';
+
+  return (
+    <div className='content-row'>
+      <div>{date}</div>
+      <div>{training.distance}</div>
+      <div>
+        <button 
+          className="content-btn"
+        >&#9998;</button>
+        <button 
+          className="content-btn"
+          onClick={() => training.date && remuveTraining(training.date)}
+        >&#10008;</button>
+      </div>
+    </div>
+  )
+}
+
+export default Training
+
+type TrainingProps = {
+  training: ITraining;
+  remuveTraining: (date: string) => void
+}
